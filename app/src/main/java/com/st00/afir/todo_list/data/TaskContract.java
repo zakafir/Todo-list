8e3162c6a1623aa1c6e6cd7bf27c00da3abda0b7
zakafir <zak.afir@gmail.com>
2017-11-15 12:54:36 +0000

    Building a Uri

--- app/src/main/java/com/st00/afir/todo_list/data/TaskContract.java
+++ app/src/main/java/com/st00/afir/todo_list/data/TaskContract.java
@@ -16,14 +16,24 @@
 
 package com.st00.afir.todo_list.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
 public class TaskContract {
 
+    public static final String AUTHORITY = "com.st00.afir.todo_list";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+AUTHORITY);
+    public static final String PATH_TASKS = "tasks";
 
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
+
+        //the final content Uri
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon()
+                        .appendPath(PATH_TASKS)
+                        .build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
