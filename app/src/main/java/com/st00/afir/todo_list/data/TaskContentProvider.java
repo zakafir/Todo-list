5b63c7c7ddb2af657a72dbe5f4fe7aedc5e17e54
zakafir <zak.afir@gmail.com>
2017-11-15 13:08:42 +0000

    Building a UriMatcher

--- app/src/main/java/com/st00/afir/todo_list/data/TaskContentProvider.java
+++ app/src/main/java/com/st00/afir/todo_list/data/TaskContentProvider.java
@@ -18,12 +18,26 @@ package com.st00.afir.todo_list.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
 public class TaskContentProvider extends ContentProvider {
 
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+    public static UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(TaskContract.AUTHORITY,TaskContract.PATH_TASKS,TASKS);
+
+        uriMatcher.addURI(TaskContract.AUTHORITY,TaskContract.PATH_TASKS+"/#",TASK_WITH_ID);
+
+        return uriMatcher;
+    }
 
     TaskDbHelper taskDbHelper;
     /* onCreate() is where you should initialize anything you’ll need to setup
